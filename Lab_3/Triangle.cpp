675d1576bd64010eef5f48fb853d303dec64b498
MFMF(MagicFoxMenFour) <135065601+MagicFoxMenFour@users.noreply.github.com>
2024-05-12 21:06:33 +0300

    Update Triangle.cpp

--- Lab_3/Triangle.cpp
+++ Lab_3/Triangle.cpp
@@ -1,1 +1,31 @@
+#include "Triangle.h"
+#include "math.h"
+#include <iostream>
 
+using namespace std;
+
+// Конструктор с параметрами для задания катетов
+RightTriangle::RightTriangle(double leg1, double leg2) : legH(leg1), legW(leg2) {}
+
+// Геттеры для катетов
+double RightTriangle::getLegH() const {
+    return legH;
+}
+
+double RightTriangle::getLegW() const {
+    return legW;
+}
+
+// Сеттеры для катетов
+void RightTriangle::setLegH(double newLegH) {
+    legH = newLegH;
+}
+
+void RightTriangle::setLegW(double newLegW) {
+    legW = newLegW;
+}
+
+// Вычисление гипотенузы
+double RightTriangle::getHypotenuse() const {
+    return sqrt(pow(legH, 2) + pow(legW, 2));
+}
