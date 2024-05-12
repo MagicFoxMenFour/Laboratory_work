18da6e311e1c33efff58ba0210b7841382c4885b
MFMF(MagicFoxMenFour) <135065601+MagicFoxMenFour@users.noreply.github.com>
2024-05-12 20:41:16 +0300

    Update ExchangeRequest.h

--- Lab_6/ExchangeRequest.h
+++ Lab_6/ExchangeRequest.h
@@ -1,1 +1,18 @@
+#ifndef EXCHANGEREQUEST_H
+#define EXCHANGEREQUEST_H
 
+#include "Apartment.h"
+
+class ExchangeRequest {
+public:
+    ExchangeRequest(const Apartment& wanted, const Apartment& available);
+
+    bool matchApartments() const; // Проверка совпадения квартир
+    void print() const; // Вывод информации о заявке
+
+private:
+    Apartment wantedApartment; // Требуемая квартира
+    Apartment availableApartment; // Доступная квартира
+};
+
+#endif // EXCHANGEREQUEST_H
