9de5eae6b2993a90fb190ef2dffdb240a39cb525
MFMF(MagicFoxMenFour) <135065601+MagicFoxMenFour@users.noreply.github.com>
2024-05-12 21:00:41 +0300

    Update Triangle.h

--- Lab_3/Triangle.h
+++ Lab_3/Triangle.h
@@ -1,1 +1,15 @@
+#pragma once
 
+class RightTriangle //Класс правильного треугольника
+{
+	public:
+		RightTriangle(); //Конструктор
+		double GetlegW(); //Получаем доступ к 1 катету
+		double GetlegH(); //Получаем доступ к 2 катету
+		void SetlegW(double legW); //Устанавливаем значение 1 ктатета
+		void SetlegH(double legH); //Устанавливаем значение 2 ктатета
+		double GetHypotenuse(); //Получаем гипотенузу
+	private:
+		double legW;
+		double legH;
+};
