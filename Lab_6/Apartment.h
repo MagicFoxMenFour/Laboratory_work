e3b48ca5a5166b9a77d32988891e5a8eb7d676d1
MFMF(MagicFoxMenFour) <135065601+MagicFoxMenFour@users.noreply.github.com>
2024-05-12 20:40:42 +0300

    Update Apartment.h

--- Lab_6/Apartment.h
+++ Lab_6/Apartment.h
@@ -1,1 +1,13 @@
+#ifndef APARTMENT_H
+#define APARTMENT_H
 
+#include <string>
+
+struct Apartment {
+    int numRooms; // Количество комнат
+    float area; // Площадь
+    int floor; // Этаж
+    std::string district; // Район
+};
+
+#endif // APARTMENT_H
