5ef349a52692de28f9a7923d8e50f5397346f822
MFMF(MagicFoxMenFour) <135065601+MagicFoxMenFour@users.noreply.github.com>
2024-05-12 20:40:58 +0300

    Update ExchangeManager.h

--- Lab_6/ExchangeManager.h
+++ Lab_6/ExchangeManager.h
@@ -1,1 +1,17 @@
+#ifndef EXCHANGEMANAGER_H
+#define EXCHANGEMANAGER_H
 
+#include "ExchangeRequest.h"
+#include <vector>
+
+class ExchangeManager {
+public:
+    void addRequest(const ExchangeRequest& request); // Добавление заявки
+    void findMatches(); // Поиск совпадений
+    void printAllRequests() const; // Вывод всех заявок
+
+private:
+    std::vector<ExchangeRequest> requests; // Список заявок
+};
+
+#endif // EXCHANGEMANAGER_H
