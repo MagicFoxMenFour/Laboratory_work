8f08a3c0a4bf5d451cc584297eb483cf3f90b8ed
MFMF(MagicFoxMenFour) <135065601+MagicFoxMenFour@users.noreply.github.com>
2024-05-12 21:00:25 +0300

    Update Fraction.h

--- Lab_3/Fraction.h
+++ Lab_3/Fraction.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <iostream>
 
+template <typename T> //Создаем шаблонный класс
+class Fraction 
+{
+public:
+	Fraction(); //Конструктор
+	double ByPercent(); //Получение процентного соотношения дроби
+	int SumNumberDenominator(); //Сумма цифр знаменателя
+	~Fraction() //Деконструктор
+	{
+		std::cout << "Объект удален" << std::endl;
+	}
+private:
+	T numerator; //Поле числителя
+	T denominator; //Поле знаменателя
+};
